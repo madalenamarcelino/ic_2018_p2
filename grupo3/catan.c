de0e3e076274a9cdb7c7d2eee09ddb0987c0cfe7
AfonsoGR <44871197+AfonsoGR@users.noreply.github.com>
2019-01-22 23:09:15 +0000

    Add files via upload

--- grupo3/catan.c
+++ grupo3/catan.c
@@ -3,7 +3,16 @@
 #include <time.h>
 #include <ctype.h> 
 #include "second.c"
-//#include "../ini.h"
+
+/**
+* @author Madalena Marcelino a21700859
+* @author Afonso Rosa a21802169
+* @date 22 January 2019
+*
+* @breif An unfinished simplified version of the game catan
+*
+* 
+*/
 
 struct land {
 	int id;
@@ -14,7 +23,20 @@ struct land {
 } land;
 
 int rollDice() {  
-							// FUNCTION TO ROLLDICE
+/**
+* @breif Function to obtain 2 random numbers between 1 and 6
+*
+* Here is where the two random dice values are created and added toguether
+* for every time the player presses the key 'r', this value is the returned
+* to be called and used further on
+* 
+* @return this function returns the added valued of the two random numbers
+*
+* @breif This line initializes a random generation without repetitions
+* @code 
+* srand((unsigned int) time(NULL));
+* @endcode
+*/
 	srand((unsigned int) time(NULL));
 
 	int die1 = 0;
@@ -28,11 +50,27 @@ int rollDice() {
 }
 
 int selection (char c) {
-							// RECOGNIZE EACH KEYPRESSED FOR EACH ACTION
+/**
+* @breif Switch cases to determine what each key that is pressed does
+*
+* @param caseR This is where the function rollDice with the two added random
+* numbers is called to be executed
+* 
+* @param caseQ This case closes the game by using the function exit from the
+* stdlib.h library
+*
+* @param caseP This case is meant to be used for calling the function where
+* the player will be able to pick which land he\she wants to use at any given
+* round of the game, yet it does not work yet
+*
+* @param caseDefault This case is in case any other input other than the ones
+* specified in the code is intered. If so then the code will loop itself back
+* to the beggining of the function
+*/
 	switch(c) {
 
 		case 'r':
-			return rollDice();	// call a dice roll on r
+			return rollDice();	
 		case 'q':
 			exit(0);
 					// exit game
@@ -48,7 +86,11 @@ int selection (char c) {
 }
 
 void memoryAlloc(struct land **table) {
-							// ALLOCATE MEMORY SPACE FOR EACH LAND OWNED 
+/**
+* @breif Function to allocate memory space for the player's lands
+*
+*
+*/ 
 
 	table =(struct land*) calloc(4, sizeof(struct land*));
 
@@ -61,6 +103,7 @@ void memoryAlloc(struct land **table) {
 
 
 void selectResources(struct land **LAND, int i, int j, int player) {
+
 
 	for(int i = 0; i < 4; i++) {
 		for(int j = 0; j < 4; j++) {
@@ -76,13 +119,22 @@ void selectResources(struct land **LAND, int i, int j, int player) {
 }
 
 void villageFinder(){
+/**
+* @breif Function that calls the second part of the code
+*
+* This secondary code is reserved purely to check if the newly picked land is
+* neighoring the previously owned land. This piece of the code was too large to
+* keep in the original file therefore it was separated
+*/
 	
 	secondResourceCode();						
 	
 }
 
 void tablePrint(){
-							// DRAW GAME TABLE
+/**
+* @breif Function to print a visual of what the catan table looks like
+*/
 		printf("+----------------+----------------+----------------+----------------+\n");
 		printf("|       #0       |       #1       |       #2       |       #3       |\n");
 		printf("| N: D           | N: D           | N: D           | N: D           |\n");
@@ -117,7 +169,9 @@ void tablePrint(){
 
 
 void giveLand(int ID, int player, struct land **table) {
-							// PLAYER IDS FOR PLAYER LANDS PICKED TO BE OWNED
+/**
+* @breif Function gives land to the player
+*/
 	for(int i = 0; i < 4; i++) {
 		for(int j = 0; j < 4; j++) {
 			if(table[i][j].id == ID) {
@@ -140,31 +194,27 @@ int main() {
 	struct land **table = NULL;
 	memoryAlloc(table);
 
-
-							// LOOP FUNCTION THAT ROLLS THE 2 DICE	
+/**
+* @breif Variable that loops the several main actions of the game
+*
+* @param systemclear clears the canvas to clean out unecessary clutter
+* 
+* @param tableprint simply calls the fuunction that prints the visual canvas
+*
+* @param print.scan this is where tha player inputs the number of the land it
+* wants to start playing with
+*
+* @param giveland calls for the function that associates the land with the given
+* player
+*
+* @param instructions these are instructions that tell the player what to do
+* followed by callings of functions to print the values necessaries to lay the game
+*/	
 	while(gameLoop <= 0) {
 		system("clear");
 		char s;
 
 		tablePrint();
-		
-		//rules
-		printf("Both players will start by choosing their starting local.\n");
-		printf("Each local/village has terrains surrounding them.\n");
-		printf("On the North(N), South(S), East(E) and West(W).\n");
-		printf("Each of these terrains are linked to a resource and a number.\n");
-		printf("The resources are:\nGrain(G), Iron(I), Brick(B), Lumber(L), Wool(W) and Desert(D).\n");
-		printf("Each round 2 dice will be rolled by the player.\n");
-		printf("Player 1 goes first.\n");
-		printf("After the dice have been rolled they will be summed.\n");
-		printf("If the sum is equal to a resource in your terrain you get said resource if not, too bad.\n\n");
-		printf("After that you have 5 options you can choose from.\n");
-		printf("YOU CAN DO THIS AS LONG AS YOU DON'T PASS YOUR TURN\n");
-		printf("THERE IS NO LIMIT TO YOUR ACTIONS AS LONG AS YOU HAVE RESOURCES\n\n");
-		printf("The game ends when one player reaches 6 points.\n");
-		printf("Village = 1 Point\nCity = 2 Points\n\n");
-		printf("To build a village you need:\n - 1 G, 1 L, 1 B and 1 W\n");
-		printf("To build a city you need:\n - 2 G, 3 I\n\n");
 
 		printf("Player 1, please pick a land to start with\n");
 		scanf("%d", &player1);
@@ -174,22 +224,24 @@ int main() {
 		giveLand(player1, 1, table);
 		giveLand(player2, 2, table);
 
-		printf("Press r to roll the dice\n");
-		printf("Press q to quit\n");
+		printf("r to roll the dice\n");
+		printf("q to quit\n");
+		printf("press p to pick a land\n");
 		printf("Dice: %d\n", sum);
 		dice = getchar();
 		s = getchar();
 		dice = tolower(dice);
 		sum = selection(dice);	
 		
-		printf("Place a village.\nPress v.\n");
-		printf("Upgrade a village to a city.\nPress c.\n");
-		printf("Trade 4 of the same resource for 1 you need.\nPress t.\n");
-		printf("Trade 10 of the same resource for 1 points.\nPress s.\n");
-		printf("End your turn.\nPress e.\n");
-
 
 	}
+
+/**
+* @note Unfortunately we were not able to finish the code  and fix all of
+* errors in time of the delivery, therefore at this point the game is not 
+* playable in the terminal. We sincerly apologise for this but have decided
+* to submit everything we have been able to do.
+*/	
 
 
 
